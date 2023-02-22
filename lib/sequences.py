7ebf76be81d295531b21009b144021bc150ae1f1
Brett de Bear <bkdebear@gmail.com>
2023-02-22 10:35:14 -0800

    wee

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    output = []
+    if length > 0:
+        output.append(0)
+    if length > 1:
+        output.append(1)
+    while len(output) < length:
+        output.append(output[-1] + output[-2])
+    print(output)
+print_fibonacci(5)
+
